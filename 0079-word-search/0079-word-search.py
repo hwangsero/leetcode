967138da1a5ff6e593a8f972c8bf227b66359735
hwangsero <45163261+hwangsero@users.noreply.github.com>
2023-06-28 07:56:34 +0900

    Time: 6391 ms (72.47%), Space: 16.3 MB (73.59%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -3,8 +3,7 @@ class Solution:
         n = len(board)
         m = len(board[0])
         visited = [[0] * m for _ in range(n)]
-        dx = [0, 0, 1, -1]
-        dy = [1, -1, 0, 0]
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
         def dfs(x, y, idx):
             if idx == len(word) - 1 and board[x][y] == word[idx]:
@@ -12,23 +11,18 @@ class Solution:
             if board[x][y] != word[idx]:
                 return False
 
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-
-                if (nx >= 0 and nx < n and ny >= 0 and ny < m and visited[nx][ny] == 0):
-                    
-                    visited[nx][ny] = 1
+            visited[x][y] = 1
+            for dx, dy in directions:
+                nx, ny =  x + dx, y + dy
+                if 0 <= nx < n and 0 <= ny < m and visited[nx][ny] == 0:
                     if dfs(nx, ny, idx + 1):
                         return True
-                    visited[nx][ny] = 0
+            visited[x][y] = 0
             return False
 
         for i in range(n):
             for j in range(m):
-                if board[i][j] == word[0]:
-                    visited = [[0] * m for _ in range(n)]
-                    visited[i][j] = 1
-                    if dfs(i, j, 0):
-                        return True
+                if board[i][j] == word[0] and  dfs(i, j, 0):
+                    return True
         return False
+
