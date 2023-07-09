ba2118e2609e2221e632e575fe5e9afae2e90d60
hwangsero <45163261+hwangsero@users.noreply.github.com>
2023-07-09 21:09:38 +0900

    Time: 1907 ms (5.03%), Space: 22.7 MB (8.03%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.py
@@ -0,0 +1,27 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        n = len(gas)
+        if n == 1:
+            return 0 if gas[0] - cost[0] >= 0 else -1
+        diff = [gas[i] - cost[i] for i in range(n)]
+        left = right = 0
+        total = diff[0]
+        while (right + 1) % n != left:
+            print(f"{left=}")
+            print(f"{right=}")
+            print(f"{total=}")
+            if total < 0:
+                left -= 1
+                if left < 0:
+                    left = n - 1
+                total += diff[left]
+            else:
+                right += 1
+                right %= n
+                total += diff[right]
+            if left == right:
+                return -1
+        if total >= 0:
+            return left
+        else: 
+            return -1
