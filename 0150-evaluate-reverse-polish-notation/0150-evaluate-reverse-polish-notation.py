deef3131f2fc8031b3c79fe78253dca8f75ef47f
hwangsero <45163261+hwangsero@users.noreply.github.com>
2023-08-03 18:48:32 +0900

    Time: 86 ms (52.10%), Space: 16.7 MB (86.36%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,14 +1,10 @@
-from typing import List
-import math
-
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
         for token in tokens:
             try:
-                # int로의 변환을 시도하여 숫자인지 판별합니다.
                 stack.append(int(token))
-            except ValueError:
+            except:
                 num1 = stack.pop()
                 num2 = stack.pop()
                 if token == "+":
@@ -18,6 +14,6 @@ class Solution:
                 elif token == "*":
                     result = num2 * num1
                 elif token == "/":
-                    result = math.trunc(num2 / num1)  # 정수 나눗셈의 결과를 얻습니다.
+                    result = math.trunc(num2 / num1)
                 stack.append(result)
         return stack.pop()
