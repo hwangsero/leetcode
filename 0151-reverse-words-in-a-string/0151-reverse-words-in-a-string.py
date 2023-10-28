9c8e9603373cfaa5e986fdedc8fa22a9bdeaef0a
hwangsero <45163261+hwangsero@users.noreply.github.com>
2023-10-28 22:33:12 +0900

    Time: 41 ms (51.92%), Space: 16.4 MB (32.35%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -1,5 +1,5 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        words = s.split()  # 공백을 기준으로 단어를 분리
-        reversed_words = words[::-1]  # 단어의 순서를 뒤집음
-        return ' '.join(reversed_words) 
+        splited = s.strip().split()
+        splited.reverse()
+        return " ".join(splited)
