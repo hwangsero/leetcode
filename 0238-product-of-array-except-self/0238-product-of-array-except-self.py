a8390f4c379eeb30a66e626557f7178d3cbb1a12
hwangsero <45163261+hwangsero@users.noreply.github.com>
2023-10-28 20:06:04 +0900

    Time: 195 ms (55.02%), Space: 23.8 MB (74.28%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,24 +1,21 @@
+from typing import List
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        zero_cnt = 0
-        zero_idx = -1
-        total_product = 1
-
-        for idx, num in enumerate(nums):
-            if num == 0:
-                if zero_cnt > 0:
-                    return [0] * len(nums)
-                zero_cnt += 1
-                zero_idx = idx
-            else:
-                total_product *= num
+        n = len(nums)
+        # output 배열 초기화. 모든 값이 1이 되게끔.
+        output = [1] * n
         
-        if zero_cnt == 1:
-            ans = [0] * len(nums)
-            ans[zero_idx] = total_product
-            return ans
-
-        ans = []
-        for num in nums:
-            ans.append(total_product // num)
-        return ans
+        # 왼쪽에서 오른쪽으로 곱해나가면서 output 업데이트
+        left_product = 1
+        for i in range(n):
+            output[i] *= left_product
+            left_product *= nums[i]
+        
+        # 오른쪽에서 왼쪽으로 곱해나가면서 output 업데이트
+        right_product = 1
+        for i in range(n-1, -1, -1):
+            output[i] *= right_product
+            right_product *= nums[i]
+        
+        return output
